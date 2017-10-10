87420952e835ed673b5297a412d81a9cd99f34b6
omrobbie <putu.roby@yahoo.com>
2017-10-10 14:08:01 +0800

    setup settings preference change and click

--- app/src/main/java/com/omrobbie/cataloguemovieuiux/feature/settings/SettingsActivity.java
+++ app/src/main/java/com/omrobbie/cataloguemovieuiux/feature/settings/SettingsActivity.java
@@ -1,10 +1,15 @@
 package com.omrobbie.cataloguemovieuiux.feature.settings;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.omrobbie.cataloguemovieuiux.R;
+
+import butterknife.BindString;
+import butterknife.ButterKnife;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -23,11 +28,56 @@ public class SettingsActivity extends AppCompatActivity {
         return true;
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment {
+    public class MyPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+
+        @BindString(R.string.key_reminder_daily)
+        String reminder_daily;
+
+        @BindString(R.string.key_reminder_upcoming)
+        String reminder_upcoming;
+
+        @BindString(R.string.key_setting_locale)
+        String setting_locale;
+
         @Override
         public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            ButterKnife.bind(this, getActivity());
+
+            findPreference(reminder_daily).setOnPreferenceChangeListener(this);
+            findPreference(reminder_upcoming).setOnPreferenceChangeListener(this);
+            findPreference(setting_locale).setOnPreferenceClickListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object o) {
+            String key = preference.getKey();
+            boolean isOn = (boolean) o;
+
+            if (key.equals(reminder_daily)) {
+                Toast.makeText(SettingsActivity.this, "Daily Reminder Notification is now " + (isOn ? "activated!" : "deactivated!"), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            if (key.equals(reminder_upcoming)) {
+                Toast.makeText(SettingsActivity.this, "Daily Reminder Notification is now " + (isOn ? "activated!" : "deactivated!"), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            String key = preference.getKey();
+
+            if (key.equals(setting_locale)) {
+                return true;
+            }
+
+            return false;
         }
     }
 }
