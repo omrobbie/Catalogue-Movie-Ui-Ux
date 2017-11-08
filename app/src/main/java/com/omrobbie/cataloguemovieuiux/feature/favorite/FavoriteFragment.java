64b4cdd5e576d9c25400ef05c2303fa011be6eb6
omrobbie <putu.roby@yahoo.com>
2017-11-08 16:07:03 +0800

    prepare connection between layout and fragment

--- app/src/main/java/com/omrobbie/cataloguemovieuiux/feature/favorite/FavoriteFragment.java
+++ app/src/main/java/com/omrobbie/cataloguemovieuiux/feature/favorite/FavoriteFragment.java
@@ -1,19 +1,30 @@
 package com.omrobbie.cataloguemovieuiux.feature.favorite;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.omrobbie.cataloguemovieuiux.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavoriteFragment extends Fragment {
 
+    private Context context;
+    private Unbinder unbinder;
+
+    @BindView(R.id.rv_favorite)
+    RecyclerView rv_favorite;
 
     public FavoriteFragment() {
         // Required empty public constructor
@@ -24,7 +35,17 @@ public class FavoriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+        context = view.getContext();
+
+        unbinder = ButterKnife.bind(this, view);
+
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
